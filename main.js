ddd0a836626a4e54057cda638b5a6e506af22b04
Arvin Fernandez <arvinf07@gmail.com>
2021-02-08 21:07:56 -0500

    Done.

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
 // Your JavaScript code goes here!
+const modal = document.getElementById('modal')
+const modalMessage = document.getElementById('modal-message')
+const likes = document.querySelectorAll('.like')
+likes.forEach( element => element.firstElementChild.addEventListener('click', handleLike) )
 
+function handleLike(event){
+  mimicServerCall()
+  .then( (resp) => {
+    if (this.innerText == FULL_HEART){
+      this.classList.remove('activated-heart')
+      this.innerText = EMPTY_HEART
+    } else{
+      this.classList.add('activated-heart')
+      this.innerText = FULL_HEART
+    }
+  })
+  //Do that modal stuff here
+  .catch( (error) => {
+    modal.classList.remove('hidden')
+    modalMessage.innerText = 'Connection down. Try again later...'
+    window.setTimeout(clearModal, 5000)
+  }) 
+}
+
+function clearModal(){
+  modal.classList.add('hidden')
+}
 
 
 
